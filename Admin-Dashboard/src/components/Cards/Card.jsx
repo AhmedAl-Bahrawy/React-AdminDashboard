e68962b499fae2f2ee0b6776195118b2c550eac3
Ahmed Al-Bahrawy <ahmedalbahrawy-2010@hotmail.com>
2025-10-10 16:04:31 +0300

    Add MainDash component to App and introduce analytics and updates data in Data.jsx

---
+++ Admin-Dashboard/src/components/Cards/Card.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Card.css";
+
+const Card = ({ title, color, barValue, value, png, series }) => {
+  return <div className="Card""></div>;
+};
+
+export default Card;
